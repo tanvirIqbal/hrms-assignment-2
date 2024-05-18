d5481f08a0ed28555dfe207f52bfe2c9d7f6ef20
TamannaSheme <syedatsheme03@gmail.com>
2024-05-18 23:15:45 +1000

    PerformanceReviewControl updated

--- src/hrms/control/PerformanceReviewControl.java
+++ src/hrms/control/PerformanceReviewControl.java
@@ -2,12 +2,57 @@ package hrms.control;
 
 import boundary.IPerformanceReviewClient;
 import entity.PerformanceReview;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PerformanceReviewControl implements IPerformanceReviewClient {
+    private List<PerformanceReview> performanceReviewList;
+
+    // Constructor
+    public PerformanceReviewControl(List<PerformanceReview> performanceReviewList) {
+        this.performanceReviewList = performanceReviewList;
+    }
 
     @Override
-    public List<PerformanceReview> createPerformanceReviews() {
-        return HardcodedPerformanceReview.getHardcodedReviews();
+    public void getEmployeePerformanceReviewDetails(int employeeID) {
+        boolean found = false;
+        for (PerformanceReview review : performanceReviewList) {
+            if (review.getEmployeeId() == employeeID) {
+                System.out.println(review.toString());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Performance review data not found");
+        }
+    }
+
+    @Override
+    public void getSubordinateEmployeePerformanceReviewDetails(int lineManagerID, int employeeID) {
+        boolean found = false;
+        for (PerformanceReview review : performanceReviewList) {
+            if (review.getLineManagerId() == lineManagerID && review.getEmployeeId() == employeeID) {
+                System.out.println(review);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Performance review data not found");
+        }
+    }
+
+    @Override
+    public void getAllSubordinateEmployeesPerformanceReviewDetails(int lineManagerID) {
+        boolean found = false;
+        for (PerformanceReview review : performanceReviewList) {
+            if (review.getLineManagerId() == lineManagerID) {
+                System.out.println(review);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Performance review data not found");
+        }
     }
 }
